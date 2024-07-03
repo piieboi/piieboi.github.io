4ab61158e1c1b54808c004bbecfda3c61a89362b
Pie <89328444+piieboi@users.noreply.github.com>
2024-07-03 02:07:09 -0400

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,8 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.pieboi.xyz`,
+    icon: "src/images/icon.ico"
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
